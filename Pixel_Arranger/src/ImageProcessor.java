892c8247e980045e6d539f7f58c639be59be9a2e
Joanakor <mark67769@gmail.com>
2022-11-20 15:35:37 +0200

    Removed redundant commented code

--- Pixel_Arranger/src/ImageProcessor.java
+++ Pixel_Arranger/src/ImageProcessor.java
@@ -12,24 +12,6 @@ public class ImageProcessor {
         BufferedImage img;
         img = ImageIO.read(f);
         ArrayList<Integer> pixelcolors = new ArrayList<>();
-//        int width = img.getWidth();
-//        int height = img.getHeight();
-//        int pixel = 0;
-//
-//        Color color;
-//        String hex;
-//        ArrayList<Integer> pixelcolors = new ArrayList<>();
-//
-//        for (int y = 0; y < height; y++) {
-//            for (int x = 0; x < width; x++) {
-//                pixel = img.getRGB(x, y);
-//                color = new Color(pixel);
-//                hex = String.format("%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
-//                pixelcolors.add((int) Long.parseLong(hex, 16));
-//            }
-//        }
-//
-//        Collections.sort(pixelcolors);
 
         pixelcolors = Sort_pixels(pixelcolors, img);
 
