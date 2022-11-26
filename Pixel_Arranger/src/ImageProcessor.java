d4d48f7382fec1b080affc55a64c21040cdf1d25
Joanakor <mark67769@gmail.com>
2022-11-26 11:35:50 +0200

    -

--- Pixel_Arranger/src/ImageProcessor.java
+++ Pixel_Arranger/src/ImageProcessor.java
@@ -11,14 +11,14 @@ public class ImageProcessor {
     public BufferedImage getSortedImage(File f) throws IOException { //Returns image with sorted
         BufferedImage img;                                           //by hex values pixels
         img = ImageIO.read(f);
-        ArrayList<Integer> pixelcolors = new ArrayList<>();
+        ArrayList<Integer> pixelColors = new ArrayList<>();
 
-        pixelcolors = sortPixelsHex(pixelcolors, img);           //selected sorting
+        pixelColors = sortPixelsHex(pixelColors, img);           //selected sorting
 
         int count = 0;
         for (int y = 0; y < img.getHeight(); y++) {                //Colors are inserted in every pixel
             for (int x = 0; x < img.getWidth(); x++) {             //one by one
-                img.setRGB(x, y, pixelcolors.get(count));
+                img.setRGB(x, y, pixelColors.get(count));
                 count++;
             }
         }
@@ -26,7 +26,20 @@ public class ImageProcessor {
     }
 
     public void printUniqueColorsPercent(BufferedImage img) throws IOException {
-        
+
+        int pixelColor = 0;
+
+        Color color;
+        String colorHexValue;
+
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                pixelColor = img.getRGB(x, y);
+                color = new Color(pixelColor);
+                colorHexValue = String.format("%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+
+            }
+        }
     }
 
     public ArrayList<Integer> sortPixelsHex(ArrayList<Integer> list, BufferedImage img) // Fills and sorts
@@ -48,29 +61,37 @@ public class ImageProcessor {
         return list;
     }
 
-    public ArrayList<Integer> sortPixelsHSB(ArrayList<Integer> list, BufferedImage img)
+    public ArrayList<Integer> sortPixelsHSB (ArrayList<Integer> list, BufferedImage img)
     {
-        int pixel = 0;
+        int pixelColor = 0;
 
         Color color;
 
-        ArrayList<float[]> hsbvalues = new ArrayList<float[]>();
+        ArrayList<float[]> hsbValues = new ArrayList<float[]>();
 
         for (int y = 0; y < img.getHeight(); y++) {
             for (int x = 0; x < img.getWidth(); x++) {
-                pixel = img.getRGB(x, y);
-                color = new Color(pixel);
+                pixelColor = img.getRGB(x, y);
+                color = new Color(pixelColor);
                 float[] hsb = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
-                hsbvalues.add(hsb);
+                hsbValues.add(hsb);
             }
         }
 
-        ValuesSorter.bubbleSort(hsbvalues);
-        for (float[] x : hsbvalues)
+        ValuesSorter.bubbleSort(hsbValues);
+        for (float[] x : hsbValues)
         {
             list.add(Color.HSBtoRGB(x[0], x[1], x[2]));
         }
         return list;
+    }
+
+    public ArrayList<Integer> getArrayWithUniqueColors (ArrayList<Integer> pixelColors){
+
+        int index = 0;
+
+        for(int i = 0; i < )
+
     }
 
 }
